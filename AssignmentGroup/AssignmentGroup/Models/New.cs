7cd0ab54339f2e6582aa4114d750a59f56c56f90
T1907MQuyet <53265406+T1907MQuyet@users.noreply.github.com>
2020-08-31 11:05:07 +0700

    assignment

---
+++ AssignmentGroup/AssignmentGroup/Models/New.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentGroup.Models
+{
+    class New
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Content
+        {
+            public string description { get; set; }
+            public string url { get; set; }
+        }
+
+        public class Root
+        {
+            public int id { get; set; }
+            public string date { get; set; }
+            public string title { get; set; }
+            public string image { get; set; }
+            public Content content { get; set; }
+        }
+
+
+    }
+}
